3170d3e2c8cd36bd56708514dfff9df2be9e7fc9
harry <parkhs93@vntgcorp.com>
2025-11-19 17:22:41 +0900

    git test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Hello Cloud Run!")
 st.write("Streamlit on Google Cloud Run ? 니켈 테스트 페이지?")
-st.write("파일정리후 두번째 테스트")
+st.write("파일정리후 두번째 테스트 git commit")
 
 # onet-vpc-13-ni-pickling   
